cb24de5a93e20acb20f461e9d38f1e618f25bb59
jeck1l <roman.borisov1994@inbox.lt>
2023-05-04 20:39:29 +0300

    Initial commit

--- src/main/java/Uzduotys.java
+++ src/main/java/Uzduotys.java
@@ -3,20 +3,20 @@ public class Uzduotys {
     public static void main(String[] args) {
         String[] tekstas1 = {"as", "labai", "esu", "nusivyles", "savimi", "sorry", "Edgarai", ":D"};
 
-        String longestWord = "";
-        int longestWordLength = 0;
+        String ilgiausiasZodis1 = "";
+        int ilgiausiaEilute = 0;
 
         for (int i = 0; i < tekstas1.length; i++) {
-            String currentWord = tekstas1[i];
-            int currentWordLength = currentWord.length();
+            String zodis2 = tekstas1[i];
+            int zodis3 = zodis2.length();
 
-            if (currentWordLength > longestWordLength) {
-                longestWord = currentWord;
-                longestWordLength = currentWordLength;
+            if (zodis3 > ilgiausiaEilute) {
+                ilgiausiasZodis1 = zodis2;
+                ilgiausiaEilute = zodis3;
             }
         }
 
-        System.out.println("Ilgiausias žodis: " + longestWord);
+        System.out.println("Ilgiausias žodis: " + ilgiausiasZodis1);
     }
 
     public static boolean tekstoEilute(String text) {
