c891d7472d7d12ce7e139af80e3e057690bb2c2d
jeck1l <roman.borisov1994@inbox.lt>
2023-05-04 20:35:53 +0300

    Initial commit

--- src/main/java/Kayak/Products.java
+++ src/main/java/Kayak/Products.java
@@ -3,19 +3,24 @@ package Kayak;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import java.sql.*;
 import java.util.List;
 
 public class Products extends KayakBase {
 
-    private DbConnection db;
-    private final By products = By.xpath("//*[@id=\"u9ts\"]/div/section[2]/div/div/div/div[2]");
+    public DbConnection db;
+    private final By products = By.cssSelector(".FNY6-title , .KzeV-description]");
 
     public Products(WebDriver driver) {
         super(driver);
         db = new DbConnection();
     }
 
+    public void goTo(){
+        driver.get("https://www.kayak.com/");
+    }
     public void kayakList() {
         List<WebElement> productlist = driver.findElements(products);
 
@@ -32,8 +37,15 @@ public class Products extends KayakBase {
         }
 
     }
-
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws SQLException {
+        DbConnection dbConnection = new DbConnection();
+        dbConnection.createTable();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        KayakBase.driver = new ChromeDriver(options);
+        Products products1 = new Products(driver);
+        products1.goTo();
+        products1.kayakList();
+        driver.quit();
     }
 }
